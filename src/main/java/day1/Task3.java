ba2c38ed563f57e086ee964250fe6dd41df6cd6b
unknown <metalheavy@inbox.ru>
2020-11-30 22:00:42 +0300

    add task day1

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -3,5 +3,10 @@ package day1;
 public class Task3 {
     public static void main(String[] args) {
 
+        for(int i=9 ;i>=0 ;i=i-1 ){
+            System.out.println("JAVA");
+
+        }
+
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,14 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
+        for (int k=3,n=1;k<30;k=k+3){
+            System.out.println(n+" x "+"3 "+"= "+k);
+            n++;
+
+
+
+
+        }
 
     }
 }
