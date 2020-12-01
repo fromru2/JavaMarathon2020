ffdfa378a310d3841eff6712466340a9ac1f0e46
unknown <metalheavy@inbox.ru>
2020-12-01 23:02:31 +0300

    add task2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,25 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите количество этажей в здании");
+        int i = s.nextInt();
+        if (i > 0 && i < 4) {
+            System.out.println("Малоэтажный дом");
+        }
+        if (i > 4 && i < 9) {
+            System.out.println("Среднеэтажный дом");
+        }
+        if (i >= 9) {
+            System.out.println("Многоэтажный  дом");
+        }
+        if (i < 0) {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
+
+
