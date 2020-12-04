e54feae5302bea8febd572b7cb64623a9cd7c14a
unknown <metalheavy@inbox.ru>
2020-12-04 04:30:36 +0300

    Add day3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println("Введите делимое и делитель");
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            double newDouble1 = scanner.nextDouble();
+            double newDouble2 = scanner.nextDouble();
+            if (newDouble2 == 0)
+
+                break;
+            System.out.println(newDouble1 / newDouble2);
+
+
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,23 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        System.out.println("Введите делимое и делитель");
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        while (i < 5) {
+            double newDouble1 = scanner.nextDouble();
+            double newDouble2 = scanner.nextDouble();
+            i++;
+            if (newDouble2 == 0) {
+                System.out.println("Деление на ноль");
+            }
+            System.out.println(newDouble1 / newDouble2);
+        }
+
 
     }
 }
+
